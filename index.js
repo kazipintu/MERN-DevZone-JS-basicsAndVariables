850b3e1dac1d0de9f8ecb9e5af4eefbd943d46e8
kazipintu <kazipintu@gmail.com>
2024-05-08 19:08:33 +0530

    MERN/Devzone/JS/basicAndVariables

---
+++ index.js
@@ -0,0 +1,62 @@
+// variable: "ability to vary" -> it has five properties
+
+/* 1. write "var" to declare a variable
+2. give a "name" to the variable
+3. use "=" (equal to) sign after that
+4. write the "value" after equal to sign
+5. write ";" semicolon after the value */
+
+//right side of the "=" (equal to) value is stored in the left side "name" of the variable.
+
+//to verify "whether this value has been assigned in the vaiable, we use comsole.log(variable name).
+
+/* 1. numeric variable or number type variable
+2. string type variable
+3. boolean type variable
+*/
+
+/* variable should be written in a single word, meaningful, camelCase */
+
+//var : globally and locally works and changeable, editabel
+//let : only used globally or only used locally (either or), changeable, editable
+//const : only globally or locally (either or), but not changebale or editable.
+
+//Data-type
+/* 1. String
+2. numeric/number type
+3. boolean (true/false) */
+
+//camelcae examples 
+var price = 100;
+tshirtPrice = 100;
+myTshirtPrice = 100;
+//console.log(myTshirtPrice);
+
+var name1 = "ryan"
+//console.log(name1);
+
+var address = "kolkata, delhi, mumbai, chennai"
+//console.log(address);
+
+//boolean
+var inbox = false;
+//console.log(inbox);
+
+var saddy = true;
+console.log(saddy);
+
+var firstVariable = "It is our first variable";
+firstVariable = "New variable";
+firstVariable = 10;
+//console.log(firstVariable);
+
+let secondVariable = "It is our second variable";
+secondVariable = 10;
+//console.log(secondVariable);
+
+const thirdVariable = "It is our third variable";
+thirdVariable = 10; // not changeable
+//console.log(thirdVariable);
+
+let a = 3;
+let b = "23";
